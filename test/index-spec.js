9600bcdf94b20d8932e552c35d00bf405d67c5b4
Kelvin Omereshone <kelvinomereshone@gmail.com>
2018-06-13 11:44:59 +0100

    Updated the test to use const

--- test/index-spec.js
+++ test/index-spec.js
@@ -1,10 +1,10 @@
-var assert = require('assert');
-var calculator = require('../index');
+const assert = require('assert');
+const calculator = require('../index');
 
 describe('calculator', function() {
     describe('add function', function() {
         it('adds numbers', function () {
-            var result = calculator.add(1, 1);
+            const result = calculator.add(1, 1);
             assert.equal(result, 2);
         });
     });
